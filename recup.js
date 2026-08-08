9223ee7aa1b837b32dfccdedcad457e598e7d255
Abdullah Al Kafi <abdullacst11@gmail.com>
2026-08-09 02:42:12 +0600

    revup JS variable, condition, array and first commit

---
+++ recup.js
@@ -0,0 +1,19 @@
+// const : value will not change
+// let: value can change
+// var: we should not use it
+
+// variable
+const name = "Abdullah Al Kafi";
+let age = 21;
+
+// condition
+let amount = 200;
+if(amount > 100){
+
+}
+else{
+
+}
+
+// array
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]

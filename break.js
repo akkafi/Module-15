3f0760d0abe771c50f503308124ceb3085c4fbd8
Abdullah Al Kafi <abdullacst11@gmail.com>
2026-08-09 17:27:08 +0600

    loop break

---
+++ break.js
@@ -0,0 +1,17 @@
+// for(let i = 0; i < 150; i ++){
+//     console.log(i);
+//     if(i >= 5){
+//         break;
+//     }
+// }
+// console.log("Life after break up")
+
+let n = 55;
+while(n > 25){
+   
+    if(n < 50){
+        break;
+    }
+    console.log(n);
+    n--;
+}

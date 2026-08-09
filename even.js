93dccf42ed2803c5fe46daa293d75cef386a619f
Abdullah Al Kafi <abdullacst11@gmail.com>
2026-08-09 10:02:12 +0600

    Js While loop task

---
+++ even.js
@@ -0,0 +1,20 @@
+// let num = 1;
+// while(num <= 10){
+//     console.log(num);
+//     if(num % 2 === 0){
+//         console.log('Even Number', num)
+//     }
+//     num++;
+// }
+
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+let message = 1;
+while(message <= 60){
+    console.log(message, "I will invest at least 6 hrs every single day for next 60 days!")
+    message ++;
+}

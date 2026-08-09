bbed190a1610574807a29e3afe7d850ef0b9b0b8
Abdullah Al Kafi <abdullacst11@gmail.com>
2026-08-09 06:15:41 +0600

    JS for...of loop

---
+++ for-of.js
@@ -0,0 +1,17 @@
+const numbers = [25, 87, 11, 24, 54, 87, 92, 57, 33];
+console.log(numbers);
+
+for(const num of numbers){
+    console.log(num)
+}
+
+const fruits = ['orange', 'apple', 'banana', 'jackfruits'];
+
+// for(){
+
+// }
+
+for( const fruit of fruits){
+    console.log(fruit);
+    console.log("I want to eat")
+}

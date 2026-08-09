2443552d49124e6321033b1ad15f0663d457bada
Abdullah Al Kafi <abdullacst11@gmail.com>
2026-08-09 10:08:51 +0600

    Js while loop task in js

--- even.js
+++ even.js
@@ -12,9 +12,45 @@
 "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
 
  */
-
-let message = 1;
+/**
+ * let message = 1;
 while(message <= 60){
     console.log(message, "I will invest at least 6 hrs every single day for next 60 days!")
     message ++;
+}
+ */
+
+
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+// let oddNum = 61;
+// while(oddNum <= 100){
+//     console.log(oddNum);
+//     if(oddNum % 2 === 1){
+//         console.log('Odd Number', oddNum)
+//     }
+//     oddNum++
+// }
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+let evenNum = 78;
+while(evenNum <= 98){
+    console.log(evenNum);
+    if(evenNum % 2 === 0){
+        console.log('even Number', evenNum)
+    }
+    evenNum++
 }

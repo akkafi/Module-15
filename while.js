b33e7afaffb32052314f448e59803548ea5e3b61
Abdullah Al Kafi <abdullacst11@gmail.com>
2026-08-09 09:44:43 +0600

    while..loop in JS

---
+++ while.js
@@ -0,0 +1,24 @@
+const number = 12;
+
+if(number < 10){
+    console.log("choto akta number paichi")
+}
+console.log("something new is cokking");
+
+
+
+// * ----------------While-------loop-----
+let num = 0; // loop variable
+while(num < 5){
+    console.log('Looooooooping', num);
+    // num = num + 1;
+    // num += 1;
+    num ++;
+}
+
+let love = 0;
+
+while(love < 10){
+console.log(love, "Happy Birthday To Me");
+love ++;
+}
